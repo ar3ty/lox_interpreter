914a3db1c2adecabc9e3ab188b5317a14bb253b9
ar3ty <ar3tyrt5y@gmail.com>
2025-07-09 16:40:45 +0200

    continue on work, string type implementation

--- test_clox.sh
+++ test_clox.sh
@@ -1,2 +1,2 @@
-gcc -DTEST_MAKE clox/test_main.c clox/test.c clox/test_compiler.c clox/chunk.c clox/debug.c clox/memory.c clox/value.c clox/vm.c clox/scanner.c clox/compiler.c -o clox/tests_clox
+gcc -DTEST_MAKE clox/test_main.c clox/test.c clox/test_compiler.c clox/chunk.c clox/debug.c clox/memory.c clox/value.c clox/vm.c clox/scanner.c clox/compiler.c clox/object.c -o clox/tests_clox
 clox/tests_clox
